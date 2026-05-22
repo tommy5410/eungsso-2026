821a9f0cf117821972ecd4f74ac2d07ade9289f3
tommy5410 <wonjaei49@gmail.com>
2026-05-23 00:21:21 +0900

    Add files via upload

---
+++ hw13/blog/views.py
@@ -0,0 +1,9 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
